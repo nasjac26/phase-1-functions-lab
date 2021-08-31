ee2e7aa1ea11ff01b705c14c7c1ef50b48359877
Jacob <Jacobonasrallah@gmail.com>
2021-08-30 20:48:17 -0700

    commit tree 91f5be75a11b0fdc20b02818e5d9689640e9ca57

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(streetNumber) {
+    if (streetNumber >= 42) {
+        return (streetNumber - 42);
+    } else {
+        return (42 - streetNumber);
+    }
+    
+}
+
+function distanceFromHqInFeet(streetNumber) {
+    return Math.abs((streetNumber - 42) * 264) 
+}
+
+function distanceTravelledInFeet(startBlock, endBlock) {
+    return Math.abs((startBlock - endBlock) * 264)
+}
+
+function calculatesFarePrice(start, destination) {
+    let distanceTraveled = Math.abs((start - destination) * 264);
+    if (distanceTraveled <= 400)
+        return 0;
+    else if(distanceTraveled > 400 && distanceTraveled <= 2000) {
+        return ((distanceTraveled - 400) * .02);
+    }
+    else if(distanceTraveled > 2000 && distanceTraveled <=2500) {
+        return 25;
+    }
+    else if(distanceTraveled > 2500) {
+        return 'cannot travel that far';
+    }
+}
